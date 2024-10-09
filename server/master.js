5b9279a992efc1d6e18b97880815e5bfcc6ea5c2
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2024-10-09 13:29:22 -0300

    -docker compose

--- server/master.js
+++ server/master.js
@@ -1,14 +1,14 @@
-const autoload = require('auto-load')
-const bodyParser = require('body-parser')
-const compression = require('compression')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const express = require('express')
-const session = require('express-session')
-const KnexSessionStore = require('connect-session-knex')(session)
-const favicon = require('serve-favicon')
-const path = require('path')
-const _ = require('lodash')
+const autoload = require('auto-load');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const express = require('express');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const favicon = require('serve-favicon');
+const path = require('path');
+const _ = require('lodash');
 
 /* global WIKI */
 
@@ -17,65 +17,65 @@ module.exports = async () => {
   // Load core modules
   // ----------------------------------------
 
-  WIKI.auth = require('./core/auth').init()
-  WIKI.lang = require('./core/localization').init()
-  WIKI.mail = require('./core/mail').init()
-  WIKI.system = require('./core/system').init()
+  WIKI.auth = require('./core/auth').init();
+  WIKI.lang = require('./core/localization').init();
+  WIKI.mail = require('./core/mail').init();
+  WIKI.system = require('./core/system').init();
 
   // ----------------------------------------
   // Load middlewares
   // ----------------------------------------
 
-  const mw = autoload(path.join(WIKI.SERVERPATH, '/middlewares'))
-  const ctrl = autoload(path.join(WIKI.SERVERPATH, '/controllers'))
+  const mw = autoload(path.join(WIKI.SERVERPATH, '/middlewares'));
+  const ctrl = autoload(path.join(WIKI.SERVERPATH, '/controllers'));
 
   // ----------------------------------------
   // Define Express App
   // ----------------------------------------
 
-  const app = express()
-  WIKI.app = app
-  app.use(compression())
+  const app = express();
+  WIKI.app = app;
+  app.use(compression());
 
   // ----------------------------------------
   // Security
   // ----------------------------------------
 
-  app.use(mw.security)
-  app.use(cors({ origin: false }))
-  app.options('*', cors({ origin: false }))
+  app.use(mw.security);
+  app.use(cors({ origin: false }));
+  app.options('*', cors({ origin: false }));
   if (WIKI.config.security.securityTrustProxy) {
-    app.enable('trust proxy')
+    app.enable('trust proxy');
   }
 
   // ----------------------------------------
   // Public Assets
   // ----------------------------------------
 
-  app.use(favicon(path.join(WIKI.ROOTPATH, 'assets', 'favicon.ico')))
+  app.use(favicon(path.join(WIKI.ROOTPATH, 'assets', 'favicon.ico')));
   app.use('/_assets/svg/twemoji', async (req, res, next) => {
     try {
-      WIKI.asar.serve('twemoji', req, res, next)
+      WIKI.asar.serve('twemoji', req, res, next);
     } catch (err) {
-      res.sendStatus(404)
+      res.sendStatus(404);
     }
-  })
+  });
   app.use('/_assets', express.static(path.join(WIKI.ROOTPATH, 'assets'), {
     index: false,
     maxAge: '7d'
-  }))
+  }));
 
   // ----------------------------------------
   // SSL Handlers
   // ----------------------------------------
 
-  app.use('/', ctrl.ssl)
+  app.use('/', ctrl.ssl);
 
   // ----------------------------------------
   // Passport Authentication
   // ----------------------------------------
 
-  app.use(cookieParser())
+  app.use(cookieParser());
   app.use(session({
     secret: WIKI.config.sessionSecret,
     resave: false,
@@ -83,61 +83,62 @@ module.exports = async () => {
     store: new KnexSessionStore({
       knex: WIKI.models.knex
     })
-  }))
-  app.use(WIKI.auth.passport.initialize())
-  app.use(WIKI.auth.authenticate)
+  }));
+  app.use(WIKI.auth.passport.initialize());
+  app.use(WIKI.auth.authenticate);
 
   // ----------------------------------------
   // GraphQL Server
   // ----------------------------------------
 
-  app.use(bodyParser.json({ limit: WIKI.config.bodyParserLimit || '1mb' }))
-  await WIKI.servers.startGraphQL()
+  app.use(bodyParser.json({ limit: WIKI.config.bodyParserLimit || '1mb' }));
+  await WIKI.servers.startGraphQL();
 
   // ----------------------------------------
   // SEO
   // ----------------------------------------
 
-  app.use(mw.seo)
+  app.use(mw.seo);
 
   // ----------------------------------------
   // View Engine Setup
   // ----------------------------------------
 
-  app.set('views', path.join(WIKI.SERVERPATH, 'views'))
-  app.set('view engine', 'pug')
+  app.set('views', path.join(WIKI.SERVERPATH, 'views'));
+  app.set('view engine', 'pug');
 
-  app.use(bodyParser.urlencoded({ extended: false, limit: '1mb' }))
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }));
 
   // ----------------------------------------
   // Localization
   // ----------------------------------------
 
-  WIKI.lang.attachMiddleware(app)
+  WIKI.lang.attachMiddleware(app);
 
   // ----------------------------------------
   // View accessible data
   // ----------------------------------------
 
-  app.locals.siteConfig = {}
-  app.locals.analyticsCode = {}
-  app.locals.basedir = WIKI.ROOTPATH
-  app.locals.config = WIKI.config
+  app.locals.siteConfig = {};
+  app.locals.analyticsCode = {};
+  app.locals.basedir = WIKI.ROOTPATH;
+  app.locals.config = WIKI.config;
   app.locals.pageMeta = {
     title: '',
     description: WIKI.config.description,
     image: '',
     url: '/'
-  }
-  app.locals.devMode = WIKI.devMode
+  };
+  app.locals.devMode = WIKI.devMode;
 
   // ----------------------------------------
   // HMR (Dev Mode Only)
   // ----------------------------------------
 
   if (global.DEV) {
-    app.use(global.WP_DEV.devMiddleware)
-    app.use(global.WP_DEV.hotMiddleware)
+    app.use(global.WP_DEV.devMiddleware);
+    app.use(global.WP_DEV.hotMiddleware);
   }
 
   // ----------------------------------------
@@ -156,25 +157,25 @@ module.exports = async () => {
       contentLicense: WIKI.config.contentLicense,
       footerOverride: WIKI.config.footerOverride,
       logoUrl: WIKI.config.logoUrl
-    }
-    res.locals.langs = await WIKI.models.locales.getNavLocales({ cache: true })
-    res.locals.analyticsCode = await WIKI.models.analytics.getCode({ cache: true })
-    next()
-  })
+    };
+    res.locals.langs = await WIKI.models.locales.getNavLocales({ cache: true });
+    res.locals.analyticsCode = await WIKI.models.analytics.getCode({ cache: true });
+    next();
+  });
 
-  app.use('/', ctrl.auth)
-  app.use('/', ctrl.upload)
-  app.use('/', ctrl.common)
+  app.use('/', ctrl.auth);
+  app.use('/', ctrl.upload);
+  app.use('/', ctrl.common);
 
   // ----------------------------------------
   // Error handling
   // ----------------------------------------
 
   app.use((req, res, next) => {
-    const err = new Error('Not Found')
-    err.status = 404
-    next(err)
-  })
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
   app.use((err, req, res, next) => {
     if (req.path === '/graphql') {
@@ -184,26 +185,42 @@ module.exports = async () => {
           message: err.message,
           path: []
         }]
-      })
+      });
     } else {
-      res.status(err.status || 500)
-      _.set(res.locals, 'pageMeta.title', 'Error')
+      res.status(err.status || 500);
+      _.set(res.locals, 'pageMeta.title', 'Error');
       res.render('error', {
         message: err.message,
         error: WIKI.IS_DEBUG ? err : {}
-      })
+      });
     }
-  })
+  });
 
   // ----------------------------------------
   // Start HTTP Server(s)
   // ----------------------------------------
 
-  await WIKI.servers.startHTTP()
+  const http = require('http');
 
-  if (WIKI.config.ssl.enabled === true || WIKI.config.ssl.enabled === 'true' || WIKI.config.ssl.enabled === 1 || WIKI.config.ssl.enabled === '1') {
-    await WIKI.servers.startHTTPS()
+  // Crear y asignar manualmente el servidor HTTP
+  WIKI.server = http.createServer(WIKI.app);
+
+  WIKI.server.listen(WIKI.config.port, () => {
+    console.log(`Servidor escuchando en el puerto ${WIKI.config.port}`);
+  });
+
+  if (WIKI.server) {
+    WIKI.server.setTimeout(300000); // Aumenta el timeout a 5 minutos
+    console.log('Timeout configurado correctamente.');
+  } else {
+    console.error('WIKI.server sigue sin estar definido.');
   }
 
-  return true
-}
+  // ----------------------------------------
+  // HTTPS (opcional)
+  if (WIKI.config.ssl.enabled === true || WIKI.config.ssl.enabled === 'true' || WIKI.config.ssl.enabled === 1 || WIKI.config.ssl.enabled === '1') {
+    await WIKI.servers.startHTTPS();
+  }
+
+  return true;
+};
